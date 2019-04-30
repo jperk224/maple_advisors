a08d2352f430a0825e81ef1b1b81c5c11d20a4ed
Jeff <jmperkinson@gmail.com>
2019-04-29 22:16:37 -0400

    initial styling from css layout template

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'partials/header.php';
+
+?>
+
+<header>
+    <nav class="zone blue sticky">
+        <ul class="main-nav">
+            <li><a href="">Link 1</a></li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li>
+            <li class="push"><a href="">Link 4</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="container"><img class="cover" src="./images/compass1.jpg"></div>
+<div class="zone blue grid-wrapper">
+    <div class="box zone"><img src=""></div>
+</div>
+<footer class="zone yellow">2019 - Jeff Perkinson</footer>
+
+<?php
+
+require 'partials/footer.php';
+
+?>
